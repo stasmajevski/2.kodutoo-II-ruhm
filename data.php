2bb7d85d27d9cb408c7d2e34a0b140369cb026a3
Stanislav Majevski <stasmajevski@users.noreply.github.com>
2016-10-11 13:22:26 +0300

    added files

---
+++ data.php
@@ -0,0 +1,106 @@
+<?php
+
+// me peame uhendama lehed, et session tootaks
+
+	require("functions.php");
+	// kui ei ole kasutajat id'd
+	if(!isset($_SESSION["userID"]))
+	{
+		//suunan sisselogimise lehele
+		header("Location: login.php");
+		
+		// teper' on ne chitaet kod dalshe
+		exit();
+	}
+	
+	//kui on ?logout aadressireal siis login valja
+	if(isset($_GET["logout"]))
+	{
+		session_destroy();
+		header("Location: login.php");
+	}
+	$msg="";
+	if(isset($_SESSION["message"]))
+	{
+		$msg = $_SESSION["message"];	
+
+			// kui uhe naitame siis kustuta ara, et parast refreshi ei naitaks
+			unset($_SESSION["message"]);
+	}
+	
+	
+	if(isset($_POST["carPlate"]) && isset($_POST["carColor"]) && !empty($_POST["carPlate"]) && !empty($_POST["carColor"]))
+	{
+		saveCar(cleanInput($_POST["carPlate"]),cleanInput($_POST["carColor"]));
+	}
+	
+	$carData = getAllCars();
+	//echo "<pre>";
+	//var_dump($carData);
+	//echo "</pre>";
+	
+?>
+<link rel="stylesheet" type="text/css" href="style.css">
+<!-- lisasin 'Nunito' fonti -->
+ <link href='http://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
+
+<?=$msg;?>
+<p>Tere tulemast <?=$_SESSION["userEmail"];?>!</p>
+<a href="?logout=1">Logi välja</a>
+
+<form method="POST">
+	<h1>Salvesta autot</h1>
+	
+    <label for="carPlate">Cars number: </label>
+	<input name="carPlate" type="text" value=""><br><br>
+    <label for="carColor">Cars color: </label>
+	<input name="carColor" type="color"><br><br>
+	
+	<input type="submit" value="Save car" class="button">
+</form>
+
+<h1>Autod</h1>
+
+<?php
+// th - table header
+// td - table data
+// tr - table row
+
+	$html = "<table style='border:1px solid;margin:0 auto;'>";
+
+	$html .="<tr>";
+		$html .="<th>id</th>";
+		$html .="<th>plate</th>";
+		$html .="<th>color</th>";
+	$html .="</tr>";
+	
+	
+	
+	// iga liikme kohta masiivis
+	foreach($carData as $item)
+	{
+		// iga auto on $c
+	$html .="<tr>";
+		$html .="<td style='border:1px solid;'>".$item->id."</td>";
+		$html .="<td style='border:1px solid;'>".$item->plate."</td>";
+		$html .="<td style='border:1px solid; padding:10px;background-color:".$item->carColor."'>".$item->carColor."</td>";
+	$html .="</tr>";
+		
+	}
+
+	$html .="</table>";
+	echo $html;
+	
+	$listHtml = "<br><br>";
+	
+	foreach($carData as $item)
+	{
+		// iga auto on $item
+		
+		$listHtml .= "<h1 style='color:".$item->carColor."'>".$item->plate."</h1>";
+		$listHtml .= "<h1>color = ".$item->carColor."</h1>";
+			
+	}
+	
+	echo $listHtml;
+?>
---
+++ functions.php
@@ -0,0 +1,216 @@
+<?php
+require("../../config.php");
+  // functions.php
+  /*
+	function sum($x,$y)
+	{
+		
+		return $x+$y;
+		
+	}
+	echo sum(44342424,324242);
+	echo "<br>";
+	echo sum(1,1);
+	echo "<br>";
+	
+	
+	function hello($name,$surname)
+	{
+		return "Tere tulemast ".$name." ".$surname;
+	}
+	
+	echo hello("Stas","Majevski");
+	*/
+	
+	
+	/******************************************/
+	//                 SIGN UP              
+	/******************************************/
+	
+	//var_dump($GLOBALS); // php muutuaja , sees on koik muutujad
+	
+	//!!!!!!!!!!
+	// see file peab olema koigil lehtedel kus tahan kasutada SESSION muutujat
+	session_start();
+	
+	
+	function signUp($email,$password,$birthday,$gender)
+	{
+		// UHENDUS
+		$database = "if16_stanislav";
+		$mysqli = new mysqli($GLOBALS["serverHost"],$GLOBALS["serverUsername"],$GLOBALS["serverPassword"], $database);
+		
+		// sqli rida
+		$stmt = $mysqli->prepare("INSERT INTO login (email,password,birthday,gender) VALUES (?,?,?,?)");
+		
+		
+		echo $mysqli->error; // !!! Kui läheb midagi valesti, siis see käsk printib viga
+		
+		// stringina üks täht iga muutuja kohta (?), mis tüüp
+		// string - s
+		// integer - i
+		// float (double) - d
+		$stmt->bind_param("ssss",$email,$password,$birthday,$gender); // sest on email ja password VARCHAR - STRING , ehk siis email - s, password - sa
+		
+		//täida käsku
+		if($stmt->execute())
+		{
+			echo "salvsestamine õnnestus";
+		}
+		else
+		{
+			echo "ERROR ".$stmt->error;
+		}
+		
+		//panen ühenduse kinni
+		$stmt->close();
+		$mysqli->close();
+	}
+	
+	function saveCar($carNumber,$carColor)
+	{
+		$database = "if16_stanislav";
+		$mysqli = new mysqli($GLOBALS["serverHost"],$GLOBALS["serverUsername"],$GLOBALS["serverPassword"], $database);
+		
+		// sqli rida
+		$stmt = $mysqli->prepare("INSERT INTO car_and_colors (plate,color) VALUES (?,?)");
+		
+		
+		echo $mysqli->error; // !!! Kui läheb midagi valesti, siis see käsk printib viga
+		
+		// stringina üks täht iga muutuja kohta (?), mis tüüp
+		// string - s
+		// integer - i
+		// float (double) - d
+		$stmt->bind_param("ss",$carNumber,$carColor); // sest on email ja password VARCHAR - STRING , ehk siis email - s, password - sa
+		
+		//täida käsku
+		if($stmt->execute())
+		{
+			echo "salvsestamine õnnestus";
+		}
+		else
+		{
+			echo "ERROR ".$stmt->error;
+		}
+		
+		//panen ühenduse kinni
+		$stmt->close();
+		$mysqli->close();
+	}
+	
+	function login ($email,$password)
+	{
+		$error = "";
+		
+		$database = "if16_stanislav";
+		$mysqli = new mysqli($GLOBALS["serverHost"],$GLOBALS["serverUsername"],$GLOBALS["serverPassword"], $database);
+		
+		// sqli rida
+		$stmt = $mysqli->prepare("SELECT id,email,password FROM login WHERE email = ?");
+		
+		
+		echo $mysqli->error; // !!! Kui läheb midagi valesti, siis see käsk printib viga
+		
+		// asenad kusimargi
+		$stmt ->bind_param("s",$email);
+		
+		//maaran vaartused muutujatesse
+		$stmt ->bind_result($id,$emailFromDb,$passwordFromDb);
+		// tehakse paring
+		$stmt ->execute();
+		
+		// kas tulid andmed andmebaasist voi mitte
+		// on toene kui on vahemalt uks vaste
+		if($stmt->fetch())
+		{
+			// oli sellise meiliga kasutaja
+			
+			//password millega kasutaja tahab sise logida
+			$hash = hash("sha512",$password); // sha512 algoritm
+			
+			if($hash == $passwordFromDb && $email == $emailFromDb)
+			{
+				echo "Kasutaja logis sisse ".$id;
+				
+				//maaran sessiooni muutujad, millele saan ligi teestelt lehtedelt
+				$_SESSION["userID"] = $id;
+				$_SESSION["userEmail"] = $emailFromDb;
+				
+				
+				$_SESSION["message"] = "<h1>Tere tulemast!</h1>";
+				
+				header("Location: data.php");
+				exit();
+				
+			}
+			else
+			{
+				$error = "vale email või parool";
+			}
+		}
+		else
+		{
+			// ei leidnud kasutajat selle meiliga
+			$error = "vale email voi parool";
+		}
+		
+		return $error;
+	}
+	
+	function getAllCars()
+	{
+		$database = "if16_stanislav";
+		$mysqli = new mysqli($GLOBALS["serverHost"],$GLOBALS["serverUsername"],$GLOBALS["serverPassword"], $database);
+		
+		// sqli rida
+		$stmt = $mysqli->prepare("SELECT id,plate,color FROM car_and_colors");
+		
+		//maaran vaartused muutujatesse
+		
+		$stmt ->bind_result($id,$plate,$color);
+		$stmt->execute();
+		
+		
+		//tekitan massiivi
+		$result=array();
+		
+		
+		//tee seda seni, kuni on rida andmeid
+		//mis vastab select lausele
+		//fetch annab andmeid uhe rea kaupa
+		while($stmt->fetch())
+		{
+			//tekitan objekti
+			$car = new StdClass();
+			
+			$car->id = $id;
+			$car->plate = $plate;
+			$car->carColor = $color;
+			
+			//echo $plate."<br>";
+			//iga kord massiivi lisan juurde nr. m2rgi
+			array_push($result,$car);
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+		return $result;
+	}
+	
+	function cleanInput($input)
+	{
+		$input = trim($input);
+		$input = htmlspecialchars($input);
+		$input = stripslashes($input);
+		
+		return $input;
+	}
+	
+	
+	
+	
+	
+	
+?>
